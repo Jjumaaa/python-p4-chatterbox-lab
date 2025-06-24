645d8e938d0b9944a49b934fc6b7463781722283
Juma <gerald.juma@student.moringaschool.com>
2025-06-24 10:13:13 +0300

    Pushed with deprecation warnings

--- server/app.py
+++ server/app.py
@@ -11,16 +11,48 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# GET /messages - return all messages ordered by created_at
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return make_response(jsonify([m.to_dict() for m in messages]), 200)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# POST /messages - create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+
+    message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+
+    db.session.add(message)
+    db.session.commit()
+
+    return make_response(message.to_dict(), 201)
+
+# PATCH /messages/<id> - update body of a message
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if "body" in data:
+        message.body = data['body']
+
+    db.session.commit()
+    return make_response(message.to_dict(), 200)
+
+# DELETE /messages/<id> - delete a message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return make_response('', 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
